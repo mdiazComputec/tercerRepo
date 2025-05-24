1f667ccfe9afc3a9a7702afa8ad4f602361a8bfe
Martin Diaz <mdiaz@Computec.com>
2025-05-23 21:06:09 -0500

    con el primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Tercer Repo",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Unpaquete pip simple de saludo",
+    author="Martin Diaz",
+    author_email="mdiaz@Computec.com",
+    url="https://github.com/mdiazComputec/tercerRepo"
+)
